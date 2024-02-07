2efeae38383784f6dff737affd2d638cf82cc56e
Spoon <beaucamps10@gmail.com>
2024-02-08 00:05:51 +1100

    maybe changed index page idk what im doing

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+    if (username === "6=W&dDQkg`43" && password === ")2AXb00z37") {
         // If the credentials are valid, show an alert box and reload the page
         alert("You have successfully logged in.");
         window.location.href = "https://spoonyyyyyyyy.github.io/main/index.html";
